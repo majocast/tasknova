bf130d6ce3bb76a87ac586b9d265b085cacc5cfc
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2023-11-12 18:42:37 -0800

    setup database.py for db connection

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
+URL_DATABASE = f'mysql+pymysql://root:{os.environ.get("DATABASE_PASS")}@localhost:3306/BlogApplication'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
